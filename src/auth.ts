1bb9a3dccb500aabd17633480844c54b80312c40
Tushar Bhosale <tusharbhosale2281@gmail.com>
2024-07-09 16:04:16 +0530

    Done

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,8 @@ import Google from "next-auth/providers/google";
 import authConfig from "./auth.config";
 import axios from "axios";
 import { currentProfile } from "./lib/current-profile";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
@@ -10,23 +12,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     signOut: "/",
   },
   callbacks: {
-    async signIn({ user }) {
-      const data = await axios.post(
-        "http://localhost:3000/api/auth/user",
-        user
-      );
-
-      return data?.data?.success;
-    },
-
     async jwt({ token, user, account, profile }) {
       return token;
     },
 
-    async session({ token, session }) {
-      session.loggedUser = token.loggedUser as Object;
+    async session({ token, session, user }) {
+      session.loggedUser = token.sub as Object;
       return session;
     },
   },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
   ...authConfig,
 });
